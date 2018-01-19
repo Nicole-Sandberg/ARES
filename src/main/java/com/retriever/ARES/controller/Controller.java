96e85c4968f8280094316fb4c8b7dd655aae74ee
daniel.kollin <daniel.kollin@retriever.no>
2018-01-19 13:27:01 +0100

    fix

--- src/main/java/com/retriever/ARES/controller/Controller.java
+++ src/main/java/com/retriever/ARES/controller/Controller.java
@@ -1,8 +1,6 @@
 package com.retriever.ARES.controller;
 
-import static java.util.Collections.singletonMap;
 
-import java.util.Map;
 
 import com.retriever.ARES.services.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.retriever.spring.config.CurrentAccount;
-import com.retriever.spring.config.CurrentEmailLogin;
 import com.retriever.spring.config.model.Account;
-import com.retriever.spring.config.model.EmailLogin;
 
 @RestController
 public class Controller {
