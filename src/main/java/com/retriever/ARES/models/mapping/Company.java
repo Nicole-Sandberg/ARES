75a5a08cfdd6351da664c343e1ed35a2d5e1281a
nicole.sandberg <nicole.sandberg@retriever.se>
2018-02-01 14:40:46 +0100

    fungerande version

--- src/main/java/com/retriever/ARES/models/mapping/Company.java
+++ src/main/java/com/retriever/ARES/models/mapping/Company.java
@@ -3,21 +3,43 @@ package com.retriever.ARES.models.mapping;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Company {
     private String companyName;
     private String orgnr;
-    private Report report;
+    private List<Report> reports;
 
     public Company(@JsonProperty("companyName") String companyName,
                    @JsonProperty("orgnr") String orgnr,
-                   @JsonProperty("report") Report report){
+                   @JsonProperty("report") List<Report> reports){
         this.companyName = companyName;
         this.orgnr = orgnr;
-        this.report= report;
+        this.reports= reports;
     }
 
+    public String getCompanyName() {
+        return companyName;
+    }
 
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 
+    public String getOrgnr() {
+        return orgnr;
+    }
 
+    public void setOrgnr(String orgnr) {
+        this.orgnr = orgnr;
+    }
+
+    public List<Report> getReports() {
+        return reports;
+    }
+
+    public void setReports(List<Report> reports) {
+        this.reports = reports;
+    }
 }
--- src/main/java/com/retriever/ARES/utils/QueryBuilderUtils.java
+++ src/main/java/com/retriever/ARES/utils/QueryBuilderUtils.java
@@ -1,11 +1,15 @@
 package com.retriever.ARES.utils;
 
 import com.retriever.ARES.models.SearchQuery;
+import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Component
@@ -13,9 +17,18 @@ public class QueryBuilderUtils {
 	@Autowired
 	private Client client;
 
+	public static QueryBuilder getMultiNestedQuery(String query) {
+		return QueryBuilders.nestedQuery(Globals.PATH_TWO,matchQuery(query),ScoreMode.None);
+	}
+
+	private static QueryBuilder matchQuery(String query) {
+		return QueryBuilders.matchQuery(Globals.MATCH_FIELD,query);
+	}
+
 	public Optional<SearchRequestBuilder> getRequestBuilderByQuery(SearchQuery query) {
 		SearchRequestBuilder builder = getBuilder();
-		builder.setQuery(query.toQueryBuilder());
+		builder.setFetchSource(Globals.FIELDS,new String[0]);
+		query.toQueryBuilder().ifPresent(builder::setQuery);
 		return Optional.of(builder);
 	}
 
@@ -26,5 +39,10 @@ public class QueryBuilderUtils {
 	private SearchRequestBuilder getBuilder(int from, int size) {
 		return client.prepareSearch().setFrom(from).setSize(size).setIndices(Globals
 				.INDEX);
+	}
+
+	public static QueryBuilder getNestedQuery(String query) {
+		return QueryBuilders.nestedQuery(Globals.PATH_ONE,getMultiNestedQuery(query), ScoreMode.None);
+
 	}
 }
