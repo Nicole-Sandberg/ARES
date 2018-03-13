15173265fe0b317113aee007a0bae9cbd4170089
nicole.sandberg <nicole.sandberg@retriever.se>
2018-03-13 11:21:26 +0100

    findbug check!

--- src/main/java/com/retriever/ARES/controller/Controller.java
+++ src/main/java/com/retriever/ARES/controller/Controller.java
@@ -3,6 +3,8 @@ import com.retriever.ARES.models.ARESCsvOutput;
 import com.retriever.ARES.models.SearchQuery;
 import com.retriever.ARES.models.SearchResponseARES;
 import com.retriever.ARES.services.ElasticsearchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,12 @@ import com.retriever.spring.config.model.Account;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.util.*;
 
 @RestController
 public class Controller {
+	private static final Logger log = LoggerFactory.getLogger(ElasticsearchService.class);
 
 	@Autowired
 	ElasticsearchService searchService;
@@ -28,31 +32,38 @@ public class Controller {
 		String header = "företag,antal träffar,abc,verksamhetbla,xyz,söksträng2\n";
 
 		List<ARESCsvOutput> results = new ArrayList<>();
-		results.add(new ARESCsvOutput("5560125793", 4, Arrays.asList("X","X","X","X")));
-		results.add(new ARESCsvOutput("5560125790", 3, Arrays.asList("X","X","O","X")));
-		results.add(new ARESCsvOutput("5560125799", 1, Arrays.asList("X","X","O","X")));
-		results.add(new ARESCsvOutput("5560125791", 1, Arrays.asList("O","O","O","X")));
-		results.add(new ARESCsvOutput("5560125792", 2, Arrays.asList("X","O","O","X")));
+		results.add(new ARESCsvOutput("5560125793",  4, Arrays.asList(
+				"X", "X", "X", "X")));
+		results.add(new ARESCsvOutput("5560125790",  3, Arrays.asList(
+				"X", "X", "O", "X")));
+		results.add(new ARESCsvOutput("5560125799",  1, Arrays.asList(
+				"X", "X", "O", "X")));
+		results.add(new ARESCsvOutput("5560125791",  1, Arrays.asList(
+				"O", "O", "O", "X")));
+		results.add(new ARESCsvOutput("5560125792",  2, Arrays.asList(
+				"X", "O", "O", "X")));
 
 		Collections.sort(results);
 		try {
 			response.setContentType("application/csv");
-			response.setHeader("content-disposition","attachment;filename =filename.csv");
+			response.setHeader("content-disposition",
+					"attachment;filename =filename.csv");
 			OutputStream out = response.getOutputStream();
-			out.write(header.getBytes());
-			final ARESCsvOutput object = new ARESCsvOutput("Object",4,Arrays.asList("X","X","X","X"));
+			out.write(header.getBytes(Charset.forName("UTF-8")));
+			final ARESCsvOutput object = new ARESCsvOutput(
+					"Object", 4, Arrays.asList("X", "X", "X", "X"));
 			results.add(object);
 			results.forEach(company -> {
 				try {
-					out.write(company.getCsvLine().getBytes());
+					out.write(company.getCsvLine().getBytes(Charset.forName("UTF-8")));
 				} catch (IOException e) {
-					e.printStackTrace();
+					log.error(e.getMessage());
 				}
 			});
 			out.flush();
 			out.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
 	}
 
