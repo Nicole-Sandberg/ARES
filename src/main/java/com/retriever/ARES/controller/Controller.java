ccb30419f0124b85d6dcd3188dcae1cbc7d32ac4
nicole.sandberg <nicole.sandberg@retriever.se>
2018-05-31 11:09:37 +0200

    Finished refactor

--- src/main/java/com/retriever/ARES/controller/Controller.java
+++ src/main/java/com/retriever/ARES/controller/Controller.java
@@ -5,8 +5,6 @@ import com.retriever.ARES.models.SearchQuery;
 import com.retriever.ARES.models.SearchResponseARES;
 import com.retriever.ARES.services.ElasticsearchService;
 import com.retriever.ARES.services.Manager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,15 +16,11 @@ import com.retriever.spring.config.CurrentAccount;
 import com.retriever.spring.config.model.Account;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 public class Controller {
-	private static final Logger log = LoggerFactory.getLogger(ElasticsearchService.class);
 
 	final
 	ElasticsearchService searchService;
@@ -38,42 +32,6 @@ public class Controller {
 		this.searchService = searchService;
 		this.manager = manager;
 	}
-
-	/**
-	 * metod som skriver ut CSV-fil till kund. sorterad efter flest träffar per sök.
-	 * sök genom test 2 searchUMEA
-	 *
-	 * @param account Account
-	 * @param response HttpServletResponse
-	 * @param results List<ARESCsvOutput>
-	 */
-//--------------------------test Umeå CSV---------------------------
-	@RequestMapping("getExcel")
-	public void getExcel(@CurrentAccount Account account,
-						HttpServletResponse response, List<ARESCsvOutput> results) {
-		String header = "Company,Year and month,Amount of hits,Hit 1,Hit 2,Hit 3\n";
-
-		Collections.sort(results);
-		try {
-			response.setContentType("application/csv");
-			response.setHeader("content-disposition",
-					"attachment;filename =arsrapport-retriever.csv");
-			OutputStream out = response.getOutputStream();
-			out.write(header.getBytes(Charset.forName("UTF-8")));
-			results.forEach(company -> {
-				try {
-					out.write(company.getCsvLine().getBytes(Charset.forName("UTF-8")));
-				} catch (IOException e) {
-					log.error(e.getMessage());
-				}
-			});
-			out.flush();
-			out.close();
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
-	}
-
 	/**
 	 * använder elasticsearch query string querybuilder som parsar query efter AND,OR,NOT
 	 *
@@ -102,47 +60,21 @@ public class Controller {
 				.orElse(new SearchResponseARES());
 
 	}
-
 	/**
 	 * metod som söker med multi nestlade querys
 	 *
 	 * @param account Account
 	 */
-
 	//-------------------------------TEST 2 umeå--------------------------------------
 	@RequestMapping(value = "searchUmea", method = RequestMethod.GET)
 	public void searchUmea(
 			@CurrentAccount Account account,
 			HttpServletResponse httpServletResponse) {
-		List<SearchQuery> queries = getUmeaQueries();
+		List<SearchQuery> queries = manager.getUmeaQueries();
 		List<SearchResponseARES> responses = queries.stream()
 				.map(this::getDataUmea).collect(Collectors.toList());
 		List<ARESCsvOutput> results = searchService.parseResults(responses);
-		getExcel(account, httpServletResponse, results);
-	}
-
-	private List<SearchQuery> getUmeaQueries() {
-		List<SearchQuery> queries = new ArrayList<>();
-		int maxHits = 1000;
-		String documentCreatedAfter = "2018-03-03T09:06:04";
-		int offset = 0;
-
-		SearchQuery query = createQuery("väsentlig osäkerhetsfaktor",
-				maxHits, documentCreatedAfter, offset);
-		queries.add(query);
-		query = createQuery("betydande tvivel",
-				maxHits, documentCreatedAfter, offset);
-		queries.add(query);
-		query = createQuery("fortsätta verksamheten",
-				maxHits, documentCreatedAfter, offset);
-		queries.add(query);
-		return queries;
-	}
-
-
-	private SearchQuery createQuery(String query, int maxHits,
-									String documentCreatedAfter, int offset) {
-		return new SearchQuery(query, maxHits, documentCreatedAfter, offset);
+		manager.getExcel(httpServletResponse, results);
 	}
 
 	private SearchResponseARES getDataUmea(SearchQuery query) {
@@ -152,14 +84,13 @@ public class Controller {
 				.orElse(new SearchResponseARES());
 
 	}
-
 	/**
 	 * metod som kör multisearch api för att göra en sökning med 34 sökord/fraser
 	 * för umeå
 	 * @param account Account
 	 * @param httpServletResponse HttpServletResponse
 	 */
-
+	/*----------------------------------------------------------------------------------*/
 	@RequestMapping(value = "multiSearch")
 	public void multiSearch(@CurrentAccount Account account,
 							HttpServletResponse httpServletResponse) {
@@ -170,7 +101,7 @@ public class Controller {
 				.orElse(new SearchResponseARES());
 
 		List<ARESCsvOutput> results = searchService.parseResults(responses);
-		getExcel(account, httpServletResponse, results);
+		manager.getExcel(httpServletResponse, results);
 	}
 
 }
