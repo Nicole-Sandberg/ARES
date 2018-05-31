12b5427ddb61b13a60e294a3d04e11cb18dba2cc
nicole.sandberg <nicole.sandberg@retriever.se>
2018-05-31 10:46:58 +0200

    Finished umea-customer request. Starting to refactor

--- src/main/java/com/retriever/ARES/controller/Controller.java
+++ src/main/java/com/retriever/ARES/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.retriever.ARES.controller;
+
 import com.retriever.ARES.models.ARESCsvOutput;
 import com.retriever.ARES.models.SearchQuery;
 import com.retriever.ARES.models.SearchResponseARES;
 import com.retriever.ARES.services.ElasticsearchService;
+import com.retriever.ARES.services.Manager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +28,21 @@ import java.util.stream.Collectors;
 public class Controller {
 	private static final Logger log = LoggerFactory.getLogger(ElasticsearchService.class);
 
-	@Autowired
+	final
 	ElasticsearchService searchService;
+	final
+	Manager manager;
 
+	@Autowired
+	public Controller(ElasticsearchService searchService, Manager manager) {
+		this.searchService = searchService;
+		this.manager = manager;
+	}
 
 	/**
 	 * metod som skriver ut CSV-fil till kund. sorterad efter flest träffar per sök.
 	 * sök genom test 2 searchUMEA
+	 *
 	 * @param account Account
 	 * @param response HttpServletResponse
 	 * @param results List<ARESCsvOutput>
@@ -41,7 +51,7 @@ public class Controller {
 	@RequestMapping("getExcel")
 	public void getExcel(@CurrentAccount Account account,
 						HttpServletResponse response, List<ARESCsvOutput> results) {
-		String header = "Företag,År och månad,Antal träffar,Träff 1,Träff 2,Träff 3\n";
+		String header = "Company,Year and month,Amount of hits,Hit 1,Hit 2,Hit 3\n";
 
 		Collections.sort(results);
 		try {
@@ -66,14 +76,15 @@ public class Controller {
 
 	/**
 	 * använder elasticsearch query string querybuilder som parsar query efter AND,OR,NOT
+	 *
 	 * @param account Account
 	 * @param query SearchQuery
-	 * ex:
-	 * "query" : "betydande tvivel",
-	"documentCreatedAfter" : "2018-03-03T09:06:04",
-	"maxHits" : 10,
-	"includeStory" : false,
-	"offset" : 0
+	 *				ex:
+	 *				"query" : "betydande tvivel",
+	 *				"documentCreatedAfter" : "2018-03-03T09:06:04",
+	 *				"maxHits" : 10,
+	 *				"includeStory" : false,
+	 *				"offset" : 0
 	 * @return ResponseEntity<SearchResponseARES>
 	 */
 //-------------- QueryStringQueryBuilder----------------------------
@@ -81,32 +92,57 @@ public class Controller {
 	public ResponseEntity<SearchResponseARES> search(@CurrentAccount Account account,
 													@RequestBody SearchQuery query) {
 		SearchResponseARES response = getData(query);
-				return new ResponseEntity<>(response, HttpStatus.OK);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	private SearchResponseARES getData(SearchQuery query) {
-				return searchService.search(query)
-								.map(response -> new SearchResponseARES(query.getOffset(),
+		return searchService.search(query)
+				.map(response -> new SearchResponseARES(query.getOffset(),
 						Collections.singletonList(response)))
-								.orElse(new SearchResponseARES());
+				.orElse(new SearchResponseARES());
 
 	}
 
 	/**
 	 * metod som söker med multi nestlade querys
+	 *
 	 * @param account Account
-	 * @param query List<SearchQuery>
 	 */
 
 	//-------------------------------TEST 2 umeå--------------------------------------
-	@RequestMapping(value = "searchUmea", method = RequestMethod.POST)
+	@RequestMapping(value = "searchUmea", method = RequestMethod.GET)
 	public void searchUmea(
-			@CurrentAccount Account account, @RequestBody List<SearchQuery> query,
+			@CurrentAccount Account account,
 			HttpServletResponse httpServletResponse) {
-				List<SearchResponseARES> responses = query.stream()
-						.map(this::getDataUmea).collect(Collectors.toList());
-				List<ARESCsvOutput> results = searchService.parseResults(responses);
+		List<SearchQuery> queries = getUmeaQueries();
+		List<SearchResponseARES> responses = queries.stream()
+				.map(this::getDataUmea).collect(Collectors.toList());
+		List<ARESCsvOutput> results = searchService.parseResults(responses);
 		getExcel(account, httpServletResponse, results);
+	}
+
+	private List<SearchQuery> getUmeaQueries() {
+		List<SearchQuery> queries = new ArrayList<>();
+		int maxHits = 1000;
+		String documentCreatedAfter = "2018-03-03T09:06:04";
+		int offset = 0;
+
+		SearchQuery query = createQuery("väsentlig osäkerhetsfaktor",
+				maxHits, documentCreatedAfter, offset);
+		queries.add(query);
+		query = createQuery("betydande tvivel",
+				maxHits, documentCreatedAfter, offset);
+		queries.add(query);
+		query = createQuery("fortsätta verksamheten",
+				maxHits, documentCreatedAfter, offset);
+		queries.add(query);
+		return queries;
+	}
+
+
+	private SearchQuery createQuery(String query, int maxHits,
+									String documentCreatedAfter, int offset) {
+		return new SearchQuery(query, maxHits, documentCreatedAfter, offset);
 	}
 
 	private SearchResponseARES getDataUmea(SearchQuery query) {
@@ -115,6 +151,26 @@ public class Controller {
 						Collections.singletonList(response)))
 				.orElse(new SearchResponseARES());
 
+	}
+
+	/**
+	 * metod som kör multisearch api för att göra en sökning med 34 sökord/fraser
+	 * för umeå
+	 * @param account Account
+	 * @param httpServletResponse HttpServletResponse
+	 */
+
+	@RequestMapping(value = "multiSearch")
+	public void multiSearch(@CurrentAccount Account account,
+							HttpServletResponse httpServletResponse) {
+		List<SearchResponseARES> responses = (List<SearchResponseARES>)
+				searchService.searchMulti()
+				.map(response -> new SearchResponseARES(0,
+						Collections.singletonList(response)))
+				.orElse(new SearchResponseARES());
+
+		List<ARESCsvOutput> results = searchService.parseResults(responses);
+		getExcel(account, httpServletResponse, results);
 	}
 
 }
